626e58108648182d05d3006a902d8b102a5e84dc
liamoehlman <liam.oehlman@sidelab.com>
2013-07-26 14:47:09 +1000

    Version bumps

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default['s3fs-fuse'][:s3_key] = ''
 default['s3fs-fuse'][:s3_secret] = ''
-default['s3fs-fuse'][:version] = '1.68'
+default['s3fs-fuse'][:version] = '1.71'
 default['s3fs-fuse'][:no_upload] = false
 default['s3fs-fuse'][:mounts] = []
 default['s3fs-fuse'][:bluepill] = false
 default['s3fs-fuse'][:maxmemory] = 100
-default['s3fs-fuse'][:fuse_url] = 'https://downloads.sourceforge.net/project/fuse/fuse-2.X/2.8.4/fuse-2.8.4.tar.gz'
+default['s3fs-fuse'][:fuse_url] = 'https://downloads.sourceforge.net/project/fuse/fuse-2.X/2.9.3/fuse-2.9.3.tar.gz'
 default['s3fs-fuse'][:fuse_mirror] = 'voxel'
